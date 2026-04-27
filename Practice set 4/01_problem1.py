882fedd0100e4e9451cfb452404e8af4b8696513
Sajan Thakur <122763572+sajanthakur28@users.noreply.github.com>
2026-04-27 11:08:33 +0530

    more

---
+++ Practice set 4/01_problem1.py
@@ -0,0 +1,22 @@
+Fruits = []
+f1 = input(" Enter Fruit Name ")
+Fruits.append(f1)
+
+f2 = input(" Enter Fruit Name ")
+Fruits.append(f2) 
+
+f3 = input(" Enter Fruit Name ")
+Fruits.append(f3) 
+
+f4 = input(" Enter Fruit Name ")
+Fruits.append(f4) 
+
+f5 = input(" Enter Fruit Name ")
+Fruits.append(f5) 
+
+f6 = input(" Enter Fruit Name ")
+Fruits.append(f6) 
+
+f7 = input(" Enter Fruit Name ")
+Fruits.append(f7) 
+print(Fruits)
---
+++ Practice set 4/03_problem3.py
@@ -0,0 +1,2 @@
+a = (43, 768, "sajan")
+a[2] = "katyayini "
